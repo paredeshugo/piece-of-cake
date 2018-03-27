4e9ca7daf08ec1348a97a24e70dad8dee70d7429
Hugo Paredes <hugo.np@gmail.com>
2018-03-27 12:50:11 +0200

    Project initialization

---
+++ bootstrap_files/bootstrap_linux.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -Lsfo build.sh https://cakebuild.net/download/bootstrapper/linux
---
+++ bootstrap_files/bootstrap_win.ps1
@@ -0,0 +1,5 @@
+
+# For security issues while using the PowerShell script, please
+# refer to the following link.
+# https://www.cakebuild.net/docs/tutorials/powershell-security
+Invoke-WebRequest https://cakebuild.net/download/bootstrapper/windows -OutFile build.ps1
